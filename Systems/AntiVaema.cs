0ddccdc97a555f80bb514193d609a283bd2f69a3
20reaper09 <zxteam27@gmail.com>
2025-01-03 14:32:10 -0500

    Vaema stuff added

---
+++ Systems/AntiVaema.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Steamworks;
+
+namespace ModOfFantasies.Systems
+{
+    public class AntiVaema : ModSystem
+    {
+        AccountID_t account;
+        public override void OnWorldLoad()
+        {
+            if (account.m_AccountID == 76561198363708479 || Main.worldName == "Vaema" || Main.LocalPlayer.name == "Vaema" || Environment.UserName == "Vaema")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    NPC.SpawnOnPlayer(Main.LocalPlayer.whoAmI, NPCID.MoonLordCore);
+                }
+                Main.ActiveWorldFileData.IsHardMode = true;
+            }
+        }
+    }
+}
